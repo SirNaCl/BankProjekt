1133018f08f163d8fd4a99d8803727feae882e18
Emil Gedenryd <emil.gedenryd@gmail.com>
2019-01-30 14:51:06 +0100

    implementerade alla metoder

--- Customer.java
+++ Customer.java
@@ -1,26 +1,42 @@
 
 public class Customer {
+	private long idNr;
+	private int custumerNr;
+	private String name;
+
 	/**
-	* Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
-	* Kunden tilldelas också ett unikt kundnummer.
-	*/
-	Customer(String name, long idNr){
-		
+	 * Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
+	 * Kunden tilldelas också ett unikt kundnummer.
+	 */
+	Customer(String name, long idNr) {
+		this.name = name;
+		this.idNr = idNr;
+		generateCustomerNr();
 	}
+
 	/** Tar reda på kundens namn. */
 	public String getName() {
-		return null;
+		return name;
 	}
+
 	/** Tar reda på kundens personnummer. */
 	public long getIdNr() {
-		return 0;
+		return idNr;
 	}
+
 	/** Tar reda på kundens kundnummer. */
 	public int getCustomerNr() {
-		return 0;
+		return custumerNr;
 	}
+
 	/** Returnerar en strängbeskrivning av kunden. */
 	public String toString() {
-		return null;
+		return "Namn: \t\t" + name + "\nPersonnummer: \t" + idNr + "\nKundnummer: \t" + custumerNr;
+	}
+	
+	//Ökar bankens kundräknare och ger kunden ett unikt id
+	private void generateCustomerNr() {
+		Bank.custumers++;
+		custumerNr = Bank.custumers;
 	}
 }
